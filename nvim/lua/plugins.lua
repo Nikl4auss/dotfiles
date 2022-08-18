baf149ea30ba48572727174115c92f2bba4be23d
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2022-08-17 21:51:41 -0300

    added remote changes

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,42 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print('Packer is not installed')
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'kyazdani42/nvim-web-devicons'
+  use 'glepnir/lspsaga.nvim'
+  use 'L3MON4D3/LuaSnip'
+  use 'hoob3rt/lualine.nvim'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/nvim-cmp'
+  use 'onsails/lspkind.nvim'
+  use 'neovim/nvim-lspconfig'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'jose-elias-alvarez/null-ls.nvim'
+  use 'MunifTanjim/prettier.nvim'
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'akinsho/nvim-bufferline.lua'
+  use 'norcalli/nvim-colorizer.lua'
+
+  use 'lewis6991/gitsigns.nvim'
+  use 'dinhhuy258/git.nvim'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+end)
--- nvim/maps.vim
+++
@@ -1,8 +0,0 @@
-"NERDTree maps
-let mapleader = ' '
-nnoremap <C-n> :NERDTree<CR>
-nnoremap <C-t> :NERDTreeToggle<CR>
-
-"tab maps
-nnoremap <leader>2 :tabNext<CR>
-nnoremap <leader>1 :tabprevious<CR>
